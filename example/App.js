e6ceab806617d662af496ebed0b09eb7610fec5e
Jordan Howlett <jordan@howlett.io>
2019-01-25 13:01:53 -0500

    Changed example a bit

--- example/App.js
+++ example/App.js
@@ -19,19 +19,15 @@ const face = require('./assets/face.png')
 
 import Onboarder, {TextEntryScreen, BasicScreen} from 'react-native-soap'
 
-// import {BasicScreen, TextEntryScreen} from './react-native-soap/screens'
-// import {SimpleButtonScreen, TextEntryScreen, PhoneNumberAuthFlow, PermissionScreen} from './src/modules'
-
 let backgroundColor = 'white'
-
 
 class View1 extends React.Component {
   render() {
     return (
-      <TouchableOpacity onPress={this.props.screenProps.next} style={{backgroundColor: 'yellow', flex: 1, justifyContent: 'center', alignContent: 'center'}}>
+      <TouchableOpacity onPress={this.props.screenProps.next} style={{backgroundColor: 'yellow', flex: 1, justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
         <Text>can we go back to that green screen?</Text>
 
-        <TouchableOpacity style={{backgroundColor: 'white', width: 400, height: 200   }} onPress={this.props.screenProps.back}>
+        <TouchableOpacity style={{backgroundColor: 'white', width: 200, height: 200, borderRadius: 50 , justifyContent: 'center', alignItems: 'center', alignContent: 'center' }} onPress={this.props.screenProps.back}>
           <Text>BACK</Text>
         </TouchableOpacity>
       </TouchableOpacity>
@@ -51,7 +47,7 @@ class View2 extends React.Component {
 
 const IntroScreen = BasicScreen({
   header: 'Welcome to SOAP!',
-  middle: 'https://i.imgur.com/2pgeXOF.jpg',
+  middle: 'https://i.imgur.com/2D4pDjZ.png',
   footer: 'Make simple screens like this'
 })
 
@@ -63,6 +59,9 @@ const FullScreen = BasicScreen({
 const JustFace = BasicScreen({
   middle: face,
   buttonText: 'Or this',
+  backgroundStyle: {
+    backgroundColor: 'yellow'
+  }
 })
 
 const te = TextEntryScreen({
@@ -70,65 +69,6 @@ const te = TextEntryScreen({
   footer: 'Which is saved for you later',
   stateKey: 'info'
 })
-
-// let PhoneAuth = PhoneNumberAuthFlow()
-
-// let BasicView2 = SimpleButtonScreen({
-//   backgroundStyle: {
-//     backgroundColor
-//   },
-//   buttonText: 'TEST',
-//   footer: 'Please continue',
-//   middle: <Image source={screenshot} style={{resizeMode: 'contain', flex: 1}} />
-// })
-
-// let BasicView3 = SimpleButtonScreen({
-//   backgroundStyle: {
-//     backgroundColor
-//   },
-//   buttonText: 'OK?'
-// })
-
-// let TextView = TextEntryScreen({
-//   backgroundStyle: {
-//     backgroundColor
-//   },
-//   stateKey: 'username',
-//   placeholder: 'test'
-// })
-
-// let PhoneView = PhoneNumberAuthFlow({
-//   backgroundStyle: {
-//     backgroundColor
-//   },
-//   buttonText: 'Send SMS',
-//   stateKey: 'phone',
-// })
-
-// let Permission = PermissionScreen({
-//   header: 'One last thing...',
-//   stateKey: 'permissions',
-//   permissions: [
-//     {
-//       permission: 'microphone',
-//       title: 'Allow Microphone',
-//       subtitle: 'So your friends can hear your beautiful voice',
-//       required: true
-//     },
-//     {
-//       permission: 'camera',
-//       title: 'Allow Camera',
-//       subtitle: 'So your friends see your pretty face',
-//       required: true
-//     },
-//     {
-//       permission: 'notifications',
-//       title: 'Allow Notifications',
-//       subtitle: 'So you know when your friends have arrived',
-//       required: false
-//     }
-//   ]
-// })
 
 const OnboarderView = Onboarder({
   Initial: {
@@ -149,24 +89,8 @@ const OnboarderView = Onboarder({
   Custom2: {
     screen: View1
   }
-  // One: {
-  //   screen: TextView
-  // },
-  // Two: {
-  //   screen: BasicView3
-  // },
-  // Phone : {
-  //   screen: PhoneView
-  // },
-  // Auth: {
-  //   screen: PhoneAuth
-  // },
-  // Per: {
-  //   screen: Permission
-  // }
 }, {
   order: ['Initial', 'Fullscreen', 'JustFace',  'Text', 'Custom', 'Custom2'],
-  // order: ['Per', 'One', 'Initial', 'Auth'],
   animation: 'push' // "slide", "push"
 })
 
@@ -190,8 +114,8 @@ export default class App extends Component {
   renderAuthView() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignContent: 'center', alignItems: 'center'}}>
-        <Text>You entered this text</Text>
-        <Text>{this.state.data.info}</Text>
+        <Text>You finished onboarding. You entered this text:</Text>
+        <Text style={{fontSize: 70}}>{this.state.data.info}</Text>
       </View>
     )
   }
